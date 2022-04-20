5f5eccf706fa2259a49b8144494094c29e0c11ac
akilvarli <61892373+akilvarli@users.noreply.github.com>
2022-04-20 16:34:03 +0300

    adding firebase authentication for netlify

--- src/contexts/user.context.js
+++ src/contexts/user.context.js
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import { createUserDocumentFromAuth, onAuthStateChangedListener } from "../utils/firebase/firebase.utils";
 export const UserContext = createContext({
     currentUser: null,
@@ -6,9 +6,45 @@ export const UserContext = createContext({
 })
 
 
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER': 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+
+    console.log('dispatched')
+
+    console.log(action)
+
+    const { type, payload } = action;
+
+    switch (type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state,
+                currentUser: payload
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer`)
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
 
-    const [currentUser, setCurrentUser] = useState(null)
+    // const [currentUser, setCurrentUser] = useState(null)
+
+    const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE)
+
+    console.log(currentUser)
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+    }
+
     const value = { currentUser, setCurrentUser }
 
     useEffect(() => {
@@ -24,3 +60,9 @@ export const UserProvider = ({ children }) => {
 
     return <UserContext.Provider value={value}>{children}</UserContext.Provider>
 }
+
+/* const userReducer = (state, action) => {
+    return (
+        currentUser:
+    )
+}  */
